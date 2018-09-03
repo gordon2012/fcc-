07f28b5cdc130f866d3839e34d809bef409ee27d
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-09-03 11:19:17 -0700

    Add color and shade to bars based on growth
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,37 +10,18 @@ req.onload = function() {
 
     const data = dataset.data.map((d,i,a) => {
         if(i > 0) {
-            d.push(Number(d[1]) - Number(a[i-1][1]));
-            d.push((Number(d[1]) < Number(a[i-1][1]) ? -1 : 1) * Number(d[1]) / Number(a[i-1][1]));
+            const curr = Number(d[1]);
+            const prev = Number(a[i-1][1]);
+            const delta = curr - prev;
+
+            d.push(delta);
+            d.push((delta / prev) * 100);
         } else {
             d.push(0);
             d.push(0);
         }
-
-        // d.push(
-        //     i > 0 ? Number(d[1]) - Number(a[i-1][1]) : 0
-        // );
-
-        // d.push(i > 0 ? Number(d[1]) - Number(a[i-1][1]) : 0);
-
         return d;
     });
-
-    console.log(data);
-
-    // console.log(data.map((d,i,a) => i > 0 ? Number(d[1]) - Number(a[i-1][1]) : 0));
-
-
-
-
-
-    data.forEach((d,i,a) => {
-        // console.log(`${i} | ${a[i][1]}   ${a[i-1]} - ${Number(d[1])}`);
-        // console.log(` ${i} | ${a[i-1]} | ${d}`);
-    });
-
-
-
 
     const svg = d3.select('.svg-wrap')
         .append('svg')
@@ -69,16 +50,13 @@ req.onload = function() {
         .attr('id', 'y-axis')
         .call(yAxis);
 
-
-    // color
+    // Bar colors
     const cScale = d3.scaleLinear()
         .domain([d3.min(data, d => d[2]), d3.max(data, d => d[2])])
         .range([0, 255]);
     console.log(cScale(-1));
 
-
-
-    // The bars
+    // Bar shapes
     svg.selectAll('rect')
         .data(data)
         .enter()
@@ -91,9 +69,6 @@ req.onload = function() {
             .attr('fill', d => d[2] < 0 ? `rgb(${cScale(-d[2])},0,0)` : d[2] > 0 ? `rgb(0,${cScale(d[2])},0)` : 'black')
             .attr('data-date', d => d[0])
             .attr('data-gdp', d => d[1])
-            // .attr('data-growth', (d,i,a) => i > 0 ? d[1][1] > a[i-1][1] ? d[1] / a[i-1][1] : -(1 - (d[1] / a[i-1][1]) ) : 0 )
-            // .attr('data-growth', (d,i,a) => i > 0 ? `${d[1]} | ${a[i-1]}` : 0)
-            // .attr('data-gdpPrev', (d,i,a) => i > 0 ? )
             .attr('data-growth', d => d[3])
             .attr('title', d => `Date: ${d[0]}, GDP: ${d[1]}`)
 }
@@ -111,10 +86,12 @@ document.addEventListener('DOMContentLoaded', () => {
             tooltip.style.left = `${event.x + ((w - 160 - 20 - event.x) > 0 ? 20 : -180)}px`;
 
             tooltip.querySelector('.tooltip-date').innerHTML = `${data.date.slice(0,4)} Q${Math.floor((Number(data.date.slice(5,7))-1)/3)+1}`;
+
             tooltip.querySelector('.tooltip-gdp').innerHTML = `$${Number(data.gdp).toLocaleString('en')}B`;
 
-            tooltip.querySelector('.tooltip-growth').innerHTML = `${data.growth}`;
-            tooltip.querySelector('.tooltip-growth').style('color', 'red');
+            let growth = data.growth < 0 ? (Math.abs(Number(data.growth))) : data.growth > 0 ? Number(data.growth) : '';
+            tooltip.querySelector('.tooltip-growth').innerHTML = `${growth.toLocaleString('en', {maximumFractionDigits: 3})}${data.growth < 0 ? '% decrease' : data.growth > 0 ? '% increase' : ''}`;
+            tooltip.querySelector('.tooltip-growth').setAttribute('style', `color: ${data.growth < 0 ? 'red' : 'green'};`);
         }
     });
 
